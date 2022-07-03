0e25c99ad3dd4db14836169400ced6349f25f25e
Stanislav <9226303141@mail.ru>
2022-07-03 13:45:06 +0500

    some mistakes

--- js/catalog.js
+++ js/catalog.js
@@ -1,16 +1,3 @@
-//..........burger....................
-
-const burger = document.querySelector(".header__burger");
-const closeBurger = document.querySelector(".menu__close");
-const menu = document.querySelector(".menu-burger");
-
-burger.addEventListener("click", () => {
-  menu.style.display = "flex";
-});
-closeBurger.addEventListener("click", () => {
-  menu.style.display = "none";
-});
-
 //...................................фильтры..........................................
 
 const filterGoods = document.querySelector(".filter-goods__title");
